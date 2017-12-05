c299a5cd224c24777c7c5a7e50e412f0cc212f97
Darius111 <32675262+Darius111@users.noreply.github.com>
2017-12-05 14:18:46 +0100

    Update Linienfahren_DariusTest.py

--- webcam/Linienfahren_DariusTest.py
+++ webcam/Linienfahren_DariusTest.py
@@ -7,7 +7,7 @@ from aufraeumen import *
 
 import cv2             #Dies ist die Bildverarbeitungsbibliothek OpenCV
 import numpy as np     #Rechnen mit vielen Zahlen in einem Array (z. B. Bilder)
-np.set_printoptions(threshold=5, precision=2) #Dies ist nur für dieses Handson wichtig (Anzeige von langen Zahlenkolonnen)
+#np.set_printoptions(threshold=5, precision=2) #Dies ist nur für dieses Handson wichtig (Anzeige von langen Zahlenkolonnen)
 
 cap = cv2.VideoCapture(0) # Input 0
 
@@ -18,24 +18,19 @@ def detection():
 img = detection()
 width = np.size(img, 1)
 ideal = width/2
-uGrenze = (ideal)-20
-oGrenze = (ideal)+20
 print("Spalten:", width)
 
-def line(img):
+def linienmitte(img):
     img_rgb = img[:, :, ::-1] #Umdrehen der Farbkanäle
     img_red = img[:,:,2]  #Alles aus der Dimension Höhe und Breite (:,:) und den Farbkanal 2
     img_green = img[:,:,1]
     img_blue = img[:,:,0]
 
-    zeile400 = img_red[400, :]
     img_redness = img_red.astype('int16') - (img_green/2+img_blue/2)
-
-    #Schnappen wir uns also mal wieder die Linie in Zeile 400
+    
     zeile400 = img_redness[400, :]
 
-    #Dann wenden wir einen Schwellwert an:
-    zeile400_bin = zeile400>60
+    zeile400_bin = zeile400>60   # Schwellwert: 60
 
     #Das Gleiche kann man auch kürzer ausdrücken:
     x = np.arange( zeile400_bin.shape[0] ) #x=0,1,2 ... N-1 (N=Anzahl von Werten in zeile400_bin)
@@ -56,10 +51,10 @@ def forward():
 
 def links(value):
     pl.ChangeDutyCycle(value*speed)
-    pr.ChangeDutyCycle(((1-value)+1)*speed)
+    pr.ChangeDutyCycle(speed)
     
 def rechts(value):
-    pl.ChangeDutyCycle(((1-value)+1)*speed)
+    pl.ChangeDutyCycle(speed)
     pr.ChangeDutyCycle(value*speed)
 
 #######
@@ -90,7 +85,7 @@ try:
             
         rounded = round(x,1)
         
-        if uGrenze <= x <= oGrenze:
+        if x == ideal:
             forward()
         if x < ideal:
             value = (x/ideal)**2
