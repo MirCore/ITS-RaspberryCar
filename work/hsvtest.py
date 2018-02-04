3ae39c04b9c9f3253f336dd693606e76497b4c28
Mirco <m.huelse@gmail.com>
2018-02-04 09:48:23 +0100

    Alte Backup Datei gelöscht

--- work/hsvtest.py
+++
@@ -1,6 +0,0 @@
-import cv2             # Dies ist die Bildverarbeitungsbibliothek OpenCV
-import numpy as np     # Rechnen mit vielen Zahlen in einem Array (z. B. Bilder)
-
-green = np.uint8([[[187,187,252]]])
-hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
-print (hsv_green)
